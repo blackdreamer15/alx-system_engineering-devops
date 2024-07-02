9516e582709d9cbbe04abd6dc84c6625401382d7
blackdreamer15 <bakijessy@gmail.com>
2024-07-02 03:06:34 +0000

    fix: Declare identity file before turning off authentication with password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,18 @@
-# Seting up my client config file
+# Seting up ssh client config file with puppet
 include stdlib
-
-file_line { 'Turn off passwd auth':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
-}
 
 file_line { 'Delare identity file':
   path               => '/etc/ssh/ssh_config',
   line               => '    IdentityFile ~/.ssh/school',
   match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+file_line { 'Turn off passwd auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
   replace            => true,
   append_on_no_match => true
 }
