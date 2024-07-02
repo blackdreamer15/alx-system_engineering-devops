4c5c778eff40e39b3d7c05dd5106bf763fa1f8dc
blackdreamer15 <bakijessy@gmail.com>
2024-07-02 02:58:28 +0000

    chore: Configure ssh client to use a specific private key by adding its identity file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,3 +7,10 @@ file_line { 'Turn off passwd auth':
   line   => '    PasswordAuthentication no',
   replace => true,
 }
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
