fa1ee8f266b431dd033156bfb56498b08b59a244
blackdreamer15 <bakijessy@gmail.com>
2024-07-02 02:57:00 +0000

    chore: Configure ssh client to refuse authentication with password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,9 @@
 # Seting up my client config file
 include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
