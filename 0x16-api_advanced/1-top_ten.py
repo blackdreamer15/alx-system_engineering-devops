a582aae3e0f8b1c7f667cf5e0236c0b1df6c652d
blackdreamer15 <bakijessy@gmail.com>
2024-08-13 22:54:56 +0000

    feat: Refactor top_ten function to use custom user agent and limit parameter

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API
-and print the titles of the first 10
-hot posts listed for a given subreddit
+function that queries the Reddit API and prints
+the titles of the first 10 hot posts
+listed for a given subreddit.
 """
 import requests
 
+
 def top_ten(subreddit):
-    """Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+    """Get top ten hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Custom user agent'}
-    params = {'limit': 10}
+    headers = {"User-agent": "Custom"}
+    params = {"limit": 10}
 
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
-      [print(child.get("data").get("title"))
-          for child in response.json().get("data").get("children")]
+        [print(child.get("data").get("title"))
+            for child in response.json().get("data").get("children")]
     else:
         print(None)
