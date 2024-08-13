0bf2f7fe6a0af1ed6d3a764010b10895e7ab9d07
blackdreamer15 <bakijessy@gmail.com>
2024-08-13 23:07:16 +0000

    refactor: Add allow_redirect parameter to request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'custom'}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
