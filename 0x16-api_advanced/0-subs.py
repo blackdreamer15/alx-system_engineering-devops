0266f845032f5fcaf9e40e4dff286ab0525c3830
blackdreamer15 <bakijessy@gmail.com>
2024-08-13 22:44:08 +0000

    chore: Add fixes to task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and returns the number of subscribers"""
+"""
+Module to query the Reddit API
+and return the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """Function that queries the Reddit API and returns the number of subscribers"""
+    """Retrieves number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'reddit_api/0-subs/1.0'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-      data = response.json()
-      return data.get('data', {}).get('subscribers', 0)
-
+        return response.json().get("data").get("subscribers")
     else:
-      return 0
+        return 0
