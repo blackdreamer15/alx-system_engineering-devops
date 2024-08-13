10922bda8316938762dcb73718d0bd319b664918
blackdreamer15 <bakijessy@gmail.com>
2024-08-13 22:34:35 +0000

    feat: Refactor code to add fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,16 +7,11 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'reddit_api/0-subs/1.0'}
 
-    try:
-      response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-      if response.status_code == 200:
-        data = response.json()
-        subscribers = data.get('data', {}).get('subscribers', 0)
+    if response.status_code == 200:
+      data = response.json()
+      return data.get('data', {}).get('subscribers', 0)
 
-        return subscribers
-      else:
-        return 0
-
-    except requests.exceptions.RequestException as e:
-        return 0
+    else:
+      return 0
