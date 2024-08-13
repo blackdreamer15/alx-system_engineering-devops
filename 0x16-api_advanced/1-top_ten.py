f4d9179557ab18de6d224c72077b65fede8227a5
blackdreamer15 <bakijessy@gmail.com>
2024-08-13 23:10:19 +0000

    Refactor task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-function that queries the Reddit API and prints
-the titles of the first 10 hot posts
-listed for a given subreddit.
+Fetches the titles of the top 10 hot posts for a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
-    """Get top ten hot posts"""
+    """Fetches the titles of the top 10 hot posts for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-agent": "Custom"}
-    params = {"limit": 10}
+    headers = {
+        'User-Agent': 'my_reddit_app/1.0 (contact@example.com)'
+    }
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    result = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        [print(child.get("data").get("title"))
-            for child in response.json().get("data").get("children")]
+    if result.status_code == 200:
+        data = result.json()
+        posts = data.get('data', {}).get('children', [])
+
+        for post in posts[:10]:
+            print(post.get('data', {}).get('title'))
     else:
         print(None)
