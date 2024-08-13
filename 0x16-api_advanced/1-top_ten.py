d1ce7e2483ee524817b34af429ea9f38b440a45a
blackdreamer15 <bakijessy@gmail.com>
2024-08-13 22:48:25 +0000

    feat: Update top_ten function to include limit parameter

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and print the titles of the first 10 hot posts listed for a given subreddit"""
+"""
+Module to query the Reddit API
+and print the titles of the first 10
+hot posts listed for a given subreddit
+"""
 import requests
 
 def top_ten(subreddit):
     """Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'my_reddit_app/1.0 (contact@example.com)'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom user agent'}
+    params = {'limit': 10}
 
-    try:
-      response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params)
 
-      if response.status_code == 200:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-
-        for post in posts:
-          print(post.get('data', {}).get('title', ''))
-      else:
-        print(None)
-
-    except requests.exceptions.RequestException as e:
+    if response.status_code == 200:
+      [print(child.get("data").get("title"))
+          for child in response.json().get("data").get("children")]
+    else:
         print(None)
