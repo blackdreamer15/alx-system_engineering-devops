49564d37143d1291418e8136b4beac629fe0925a
blackdreamer15 <bakijessy@gmail.com>
2024-08-15 00:16:30 +0000

    fix: Add fixes to task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,23 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Returns the total number of subscribers on a given subreddit.
+    If an invalid subreddit is given, the function returns 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 404:
+        return 0
+    elif response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
